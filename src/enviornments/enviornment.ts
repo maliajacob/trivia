75801265a87cfcf3217e05a42001f6af71b0704b
CamdenCall <camden.call3036@stu.mtec.edu>
2025-04-18 09:31:37 -0600

    Initialized firebase connection

---
+++ src/enviornments/enviornment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: "trivia-7ed24",
+    appId: "1:1002185242692:web:9af116121022e1d674a411",
+    storageBucket: "trivia-7ed24.firebasestorage.app",
+    apiKey: "AIzaSyC2r2uSkCrrbo8rXRmhKl2a2oaaOTMrw64",
+    authDomain: "trivia-7ed24.firebaseapp.com",
+    messagingSenderId: "1002185242692"
+  }
+}
